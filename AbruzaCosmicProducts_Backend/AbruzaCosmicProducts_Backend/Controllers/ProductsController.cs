28c9f003ddc5fe5689115dec7730a3b8ee28598f
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-05-17 10:30:14 -0600

    add deleteProduct, addProduct etc

--- AbruzaCosmicProducts_Backend/AbruzaCosmicProducts_Backend/Controllers/ProductsController.cs
+++ AbruzaCosmicProducts_Backend/AbruzaCosmicProducts_Backend/Controllers/ProductsController.cs
@@ -20,5 +20,52 @@ namespace AbruzaCosmicProducts_Backend.Controllers
         public async Task<ActionResult<List<Product>>> getAllProducts() {
             return Ok(await context.Product.ToListAsync());
         }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> SearchProduct(int id)
+        {
+            var product = await context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(Product product)
+        {
+            context.Product.Add(product);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(getAllProducts), new { id = product.Id }, product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProductById(int id)
+        {
+            var product = await context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            context.Product.Remove(product);
+            await context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteAll()
+        {
+            context.Product.RemoveRange(context.Product);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
     }
 }
